5ba6186d24bffc4870211a101d2d1dac430c3600
Markkdc <mark.dc2305@gmail.com>
2024-07-30 18:20:05 -0700

    Add files via upload

--- project - jul 29/js/script.js
+++ project - jul 29/js/script.js
@@ -1,29 +1,17 @@
-var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+// Sample JavaScript for interactive features
 
-    breakpoints:{
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
-    },
-  });
+// Function to add item to cart
+function addToCart(productId) {
+    // Normally you would handle adding items to cart here
+    let productName = `Product ${productId}`;
+    let cartItems = document.getElementById('cartItems');
+    let li = document.createElement('li');
+    li.textContent = productName;
+    cartItems.appendChild(li);
+}
+
+// Function for checkout process (dummy function)
+function checkout() {
+    alert('Redirecting to checkout page...');
+    // Actual checkout process would be implemented here
+}
